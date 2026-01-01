25a77d6c13ee16758a38877d0e083f9e65e2af37
Louis Sanyenni <lsanyenni@gmail.com>
2026-01-01 01:33:37 +0000

    Remove __pycache__ and test.rest from repository and update .gitignore

--- photo_vault/models.py
+++ photo_vault/models.py
@@ -10,7 +10,7 @@ class Album(models.Model):
     album_name=models.CharField(max_length=25,null=True,blank=True)
     
     def __str__(self):
-        return self.album_name
+        return self.album_name or "Unnamed Album"
 
 class Photo(models.Model):
     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
