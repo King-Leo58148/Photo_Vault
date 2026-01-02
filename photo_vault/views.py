97826b18674ee461ad0f4a4c003b05addb6b22b4
Louis Sanyenni <lsanyenni@gmail.com>
2026-01-02 21:33:11 +0000

    removed redudant import from views.py

--- photo_vault/views.py
+++ photo_vault/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,get_object_or_404,get_list_or_404
 from rest_framework.authentication import authenticate,TokenAuthentication
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view,permission_classes,authentication_classes
 from rest_framework.authtoken.models import Token
 from rest_framework import status
